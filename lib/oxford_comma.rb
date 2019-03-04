1efb671ff08fe8f4ad1af14d33c118e3f6b550f7
Zoncharne Lee <zoncharne@gmail.com>
2019-03-04 06:19:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
+    elsif array.legnth == 2
+    array.join " and "
   elsif array.length > 2
     array.last.prepend "and "
     array.join ", "

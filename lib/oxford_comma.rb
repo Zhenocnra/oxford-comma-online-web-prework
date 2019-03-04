7706479e58c105aca6535e6ca0d4d19f58ad43a7
Zoncharne Lee <zoncharne@gmail.com>
2019-03-04 06:21:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
-  elsif array.legnth == 2
+  elsif array.length == 2
     array.join " and "
   elsif array.length > 2
     array.last.prepend "and "
